8cdcffa865827f52e4c7e878795837fac79f54bf
DK\psk <psk@sitecore.net>
2017-06-14 17:30:05 -0400

    Initial fix

---
+++ src/Sitecore.Support.167837/ContentSearch/Analytics/Pipelines/TranslateQuery/AnalyticsContextResolver.cs
@@ -0,0 +1,43 @@
+﻿namespace Sitecore.Support.ContentSearch.Analytics.Pipelines.TranslateQuery
+{
+    using Sitecore.Analytics;
+    using System.Collections.Generic;
+    using System.Text;
+    using System.Linq;
+    using Sitecore.ContentSearch.Pipelines.TranslateQuery;
+    using Sitecore.ContentSearch.Utilities;
+    using Sitecore.Xdb.Configuration;
+    public class AnalyticsContextResolver : Sitecore.ContentSearch.Analytics.Pipelines.TranslateQuery.AnalyticsContextResolver
+    {
+        protected override IEnumerable<SearchStringModel> Search(TranslateQueryArgs args)
+        {
+            if (XdbSettings.Tracking.Enabled)
+            {
+                for (int i = 0; i < args.SearchStringModel.Count<SearchStringModel>(); i++)
+                {
+                    if (Tracker.Current != null && Tracker.Current.Interaction != null && Tracker.Current.Interaction.LocationReference != null)
+                    {
+                        args.SearchStringModel.ElementAt(i).Value = args.SearchStringModel.ElementAt(i).Value.Replace("$CurrentLocation", Tracker.Current.Interaction.LocationReference.Country);
+                        args.SearchStringModel.ElementAt(i).Value = args.SearchStringModel.ElementAt(i).Value.Replace("$ExternalUser", Tracker.Current.Interaction.LocationReference.Identifier);
+                        args.SearchStringModel.ElementAt(i).Value = args.SearchStringModel.ElementAt(i).Value.Replace("$AreaCode", Tracker.Current.Interaction.GeoData.AreaCode);
+                        args.SearchStringModel.ElementAt(i).Value = args.SearchStringModel.ElementAt(i).Value.Replace("$BrowserName", Tracker.Current.Interaction.BrowserInfo.BrowserMajorName);
+                        args.SearchStringModel.ElementAt(i).Value = args.SearchStringModel.ElementAt(i).Value.Replace("$CurrentCity", Tracker.Current.Interaction.GeoData.City);
+                        args.SearchStringModel.ElementAt(i).Value = args.SearchStringModel.ElementAt(i).Value.Replace("$DeviceId", Tracker.Current.Interaction.DeviceId.ToString());
+                        if (Tracker.Current.Interaction.Ip != null)
+                        {
+                            args.SearchStringModel.ElementAt(i).Value = args.SearchStringModel.ElementAt(i).Value.Replace("$GeoIP", Encoding.Default.GetString(Tracker.Current.Interaction.Ip));
+                        }
+                        else
+                        {
+                            Diagnostics.Log.Warn(
+                                "Cannot resolve context GeoIP for current interaction " +
+                                Tracker.Current.Interaction.InteractionId, this);
+                        }
+                        args.SearchStringModel.ElementAt(i).Value = args.SearchStringModel.ElementAt(i).Value.Replace("$UserAgentID", Tracker.Current.Interaction.UserAgent);
+                    }
+                }
+            }
+            return args.SearchStringModel;
+        }
+    }
+}
---
+++ src/Sitecore.Support.167837/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.167837")]
+[assembly: AssemblyProduct("Sitecore.Support.167837")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyDescription("Sitecore may fail to create a search query if IP assosiated with a visit is null")]
+
